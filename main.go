9e0a34fb052252555f91884a1f98472f58aeec61
Anurag Tryambak Raut <120129433+AnuragRaut08@users.noreply.github.com>
2025-02-06 16:38:21 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
     "fmt"
     "os"
-    "path/filepath"
-
-    
+    "fynd-init-smalldata-pipeline/cmd/project"
 )
 
 func main() {
@@ -16,37 +14,8 @@ func main() {
 
     projectName := os.Args[1]
 
-    // Create the main project directory
-    err := os.MkdirAll(projectName, 0755)
-    if err != nil {
-        fmt.Printf("Error creating project directory: %v\n", err)
-        return
+    if err := project.InitProject(projectName); err != nil {
+        fmt.Printf("Failed to initialize project: %v\n", err)
+        os.Exit(1)
     }
-
-    // Create the asset subdirectory
-    assetDir := filepath.Join(projectName, "asset")
-    err = os.MkdirAll(assetDir, 0755)
-    if err != nil {
-        fmt.Printf("Error creating asset directory: %v\n", err)
-        return
-    }
-
-    // Create project.yml with name and version
-    projectYmlPath := filepath.Join(projectName, "project.yml")
-    projectYmlContent := fmt.Sprintf("name: %s\nversion: 1.0.0\n", projectName)
-    err = os.WriteFile(projectYmlPath, []byte(projectYmlContent), 0644)
-    if err != nil {
-        fmt.Printf("Error creating project.yml: %v\n", err)
-        return
-    }
-
-    // Create an empty pipeline.yml file
-    pipelineYmlPath := filepath.Join(projectName, "pipeline.yml")
-    err = os.WriteFile(pipelineYmlPath, []byte{}, 0644)
-    if err != nil {
-        fmt.Printf("Error creating pipeline.yml: %v\n", err)
-        return
-    }
-
-    fmt.Printf("Project '%s' has been initialized successfully.\n", projectName)
 }
